61551052ac8559beec2d2f347285c4fe257c3d5a
xm <dxp157@sohu.com>
2019-02-16 16:37:00 +0800

    初始化项目

---
+++ pages/index/index.js
@@ -0,0 +1,13 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  
+})
