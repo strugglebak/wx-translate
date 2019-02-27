8f359c3dea29336ac7adb130851943b7f3073f9d
moss <dxp157@sohu.com>
2019-02-27 14:33:00 +0800

    记录历史功能优化，不重复记录一模一样的 query 和 result

--- pages/index/index.js
+++ pages/index/index.js
@@ -14,6 +14,7 @@ Page({
     hideCloseIcon: true,
     query: '',
     translateResult: '',
+    prefix: {}
   },
 
   /**
@@ -65,11 +66,17 @@ Page({
     ).then(data=> {
       console.log(data)
       let history = wx.getStorageSync('history') || []
+      if (history.length) {
+        this.setData({
+          prefix: history[0]
+        })
+      }
+
       let resultArray = []
       for (let i=0; i<data.trans_result.length; i++) {
         resultArray.push(data.trans_result[i])
       }
-      console.log(resultArray)
+      console.log('resultArray', resultArray)
       let src = []
       let dst = []
       resultArray.forEach(key=> {
@@ -84,10 +91,24 @@ Page({
         translateResult: resultString
       })
 
-      history.unshift({
-        query: queryString,
-        result: resultString
-      })
+      if (history.length === 0) {
+        history.unshift({
+          query: queryString,
+          result: resultString
+        })
+      } else {
+        let prefix = this.data.prefix
+        console.log('prefix', prefix)
+        if (prefix && prefix.query && prefix.result
+          && queryString !== prefix.query
+          && resultString !== prefix.result) {
+          history.unshift({
+            query: queryString,
+            result: resultString
+          })
+        }
+      }
+
       history.length = history.length > 10 ? 10 : history.length
       wx.setStorageSync('history', history)
     }, error=> {
